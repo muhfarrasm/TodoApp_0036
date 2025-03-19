d9cb5474a47c2a5a659d6da45412cfdb55c34af8
muhfarrasm <muhammadfarrasm@gmail.com>
2025-03-19 11:28:01 +0700

    Membuat StatefulWidget

---
+++ lib/form_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class FormPage extends StatefulWidget {
+  const FormPage({super.key});
+
+  @override
+  State<FormPage> createState() => _FormPageState();
+}
+
